34404d2bc8a66244f9db6a587ccb80995bc77528
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-03-11 01:38:07 +0500

    connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db.js')
 const products = require('./data/products')
+const colors = require('colors')
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
@@ -22,5 +25,8 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(
    PORT,
-   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+   console.log(
+      `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+         .bold
+   )
 )
