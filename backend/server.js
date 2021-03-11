7714e3c42a2ff9c7c8c23b768b0f3e212a250619
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-03-11 19:39:03 +0500

    error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db.js')
-const products = require('./data/products')
+const middleware = require('./middleware/errorMiddleware')
+const { notFound, errorHandler } = middleware
+console.log(middleware)
 const colors = require('colors')
+
+const productRoutes = require('./routes/productRoutes')
 
 dotenv.config()
 connectDB()
 
 const app = express()
 
+app.use('/api/products', productRoutes)
+
+//Middlewares
+app.use(notFound)
+app.use(errorHandler)
+
 app.get('/', (req, res) => {
    res.send('API is running...')
-})
-
-app.get('/api/products', (req, res) => {
-   res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-   const product = products.find((p) => p._id === req.params.id)
-   res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
