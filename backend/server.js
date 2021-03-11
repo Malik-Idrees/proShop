99f2d7e7de4fd01a69e01d3e7ba4d38abb73221b
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-03-12 00:51:08 +0500

    Product list reducer, action & HomeScreen

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv')
 const connectDB = require('./config/db.js')
 const middleware = require('./middleware/errorMiddleware')
 const { notFound, errorHandler } = middleware
-console.log(middleware)
 const colors = require('colors')
 
 const productRoutes = require('./routes/productRoutes')
