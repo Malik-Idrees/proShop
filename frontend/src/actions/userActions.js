e5d4e3fce9face74d279b301005909a1d800d9ca
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-03-20 01:45:38 +0500

    User login & logout FUnctionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import {
+   USER_LOGIN_FAIL,
    USER_LOGIN_REQUEST,
    USER_LOGIN_SUCCESS,
+   USER_LOGOUT,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -30,11 +32,16 @@ export const login = (email, password) => async (dispatch) => {
       localStorage.setItem('userInfo', JSON.stringify(data))
    } catch (error) {
       dispatch({
-         type: PRODUCT_DETAILS_FAIL,
+         type: USER_LOGIN_FAIL,
          payload:
             error.response && error.response.data.message
                ? error.response.data.message
                : error.message,
       })
    }
+}
+
+export const logout = () => (dispatch) => {
+   localStorage.removeItem('userInfo')
+   dispatch({ type: USER_LOGOUT })
 }
