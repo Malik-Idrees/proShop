c8e03bbe432985f0f58dc2c318deafcd074e835b
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-03-20 17:13:29 +0500

    User profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,11 @@ import { productDetailsReducer } from './reducers/productReducers'
 
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer } from './reducers/userReducers'
-import { userRegisterReducer } from './reducers/userReducers'
+import {
+   userRegisterReducer,
+   userDetailsReducer,
+   userUpdateProfileReducer,
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
    productList: productListReducer,
@@ -14,6 +18,8 @@ const reducer = combineReducers({
    cart: cartReducer,
    userLogin: userLoginReducer,
    userRegister: userRegisterReducer,
+   userDetails: userDetailsReducer,
+   userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
